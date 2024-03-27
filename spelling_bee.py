f9423e58682676f081d0877bad3126e866e77561
Arun K Viswanathan <github@element77.com>
2024-03-27 20:48:28 +0530

    Code simplification

--- spelling_bee.py
+++ spelling_bee.py
@@ -13,23 +13,19 @@ import argparse
 
 def parse_arguments():
     """
-    Parse command-line arguments for the Spelling Bee puzzle.
+     Parse command-line arguments for the Spelling Bee puzzle.
 
-    The arguments include the center letter, other letters, minimum word length, and the
-    dictionary file for the Spelling Bee puzzle.
+     The arguments include the center letter, other letters, minimum word length, and the
+     dictionary file for the Spelling Bee puzzle.
 
-    Returns:
-    Namespace: An argparse.Namespace object with the parsed arguments as attributes.
-    """
+     Returns:
+     Namespace: An argparse.Namespace object with the parsed arguments as attributes.
+     """
     parser = argparse.ArgumentParser(description='Spelling Bee')
-    parser.add_argument('-c', '--center', type=str, required=True,
-                        help='Center letter')
-    parser.add_argument('-o', '--other', type=str, required=True,
-                        help='Other letters')
-    parser.add_argument('-m', '--min', type=int, default=4,
-                        help='Minimum word length')
-    parser.add_argument('-d', '--dict', type=str, required=True,
-                        help='Dictionary file')
+    parser.add_argument('-c', '--center', type=str, required=True, help='Center letter')
+    parser.add_argument('-o', '--other', type=str, required=True, help='Other letters')
+    parser.add_argument('-m', '--min', type=int, default=4, help='Minimum word length')
+    parser.add_argument('-d', '--dict', type=str, required=True, help='Dictionary file')
     return parser.parse_args()
 
 
@@ -41,14 +37,12 @@ if __name__ == '__main__':
     except FileNotFoundError:
         print(f"File {args.dict} not found.")
         word_list = []
-    center_letter_set = set(args.center.lower())
-    letters_set = center_letter_set.union(set(args.other.lower()))
-    valid_words = [word for word in word_list
-                   if center_letter_set.issubset(word)
-                   and set(word).issubset(letters_set)
-                   and len(word) >= args.min]
-    words = sorted(valid_words)
-    pangrams = [word for word in words if set(word) == letters_set]
-    print(f'Total words: {len(words)}')
-    print(f'Pangrams: {pangrams}')
-    print(f'Valid words: {words}')
+
+    all_letters = set(args.center.lower()).union(set(args.other.lower()))
+    words = sorted([word for word in word_list
+                    if set(args.center.lower()).issubset(word)
+                    and set(word).issubset(all_letters)
+                    and len(word) >= args.min])
+    pangrams = [word for word in words if set(word) == all_letters]
+
+    print(f'Total words: {len(words)}\nPangrams: {pangrams}\nValid words: {words}')
